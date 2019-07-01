b1317ce899301279e770ce040aaf1e868765df8d
kanika0212 <47709010+kanika0212@users.noreply.github.com>
2019-07-01 14:16:36 -0400

    Longest Common Substring

---
+++ LongestCommonString.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string.h>
+
+class LongestCommonSubstring{
+    private:
+	std::string s1;
+	public:
+
+	LongestCommonSubstring(std::string str){
+		s1 = str;
+	}
+	int FindLongestCommonSubstringLength(std::string f1){
+		int DynProg[s1.length()][f1.length()] = {0}, max=0,end;
+		for (int i=0;i<s1.length();i++){
+			for (int j=0;j<f1.length();j++){
+				if (s1[i]==f1[j]){
+					DynProg[i][j] =  DynProg[i-1][j-1] +1;
+					if (max < DynProg[i][j])
+										end = i;
+					max = std::max(max,DynProg[i][j]);
+ 
+
+				}
+
+			}
+		}
+		std::string X;
+		X = s1.substr(end - max +1 , max);
+		std::cout<<" substr = "<<X<<std::endl;
+		return max;
+	}
+};
+
+int main() {
+  string String("kanika"),String2("aniak");
+	LongestCommonSubstring x(String);
+	cout<< "length = "<<x.FindLongestCommonSubstringLength(String2)<<endl;
+  return 0;
+}
